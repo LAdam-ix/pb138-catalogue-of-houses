2887eb10c9689b296a81be0688e79aedc3d0c897
Martin Gallik <xgallik@mail.muni.cz>
2023-04-29 12:06:42 +0200

    fix(components): moves main header to separate component

--- frontend/src/components/main/index.tsx
+++ frontend/src/components/main/index.tsx
@@ -1,37 +1,8 @@
-import { Row, Col, Card, Image, Space } from "antd";
 import "../../assets/styles/main.css";
-import logo from "../../assets/images/logo.svg";
-import headerImage from "../../assets/images/header-image.png";
-import { Count } from "./Count";
+import { Header } from "./Header";
 
 export const MainIndex = () => {
   return (
-    <Row justify="center">
-      <Col span={24}>
-        <Card className="bg-gradient">
-          <Row>
-            <Col span={24} lg={{ span: 8, offset: 4 }} className="color-light-gray">
-              <Space direction='vertical' size='large'>
-                <Row>
-                  <Col span={15}>
-                    <Image src={logo} alt="Logo" />
-                  </Col>
-                </Row>
-                <Row>
-                  <h1 className="title">Find A Perfect Home<br />For Your Family</h1>
-                </Row>
-                <Row>
-                  <Count number={100} text="DESIGNERS"></Count>
-                  <Count number={1000} text="DESIGNES"></Count>
-                </Row>
-              </Space>
-            </Col>
-            <Col span={0} lg={{ span: 8, offset: 0 }} style={{ position: "relative" }}>
-              <img src={headerImage} alt="Header image" className="main-image" />
-            </Col>
-          </Row>
-        </Card>
-      </Col>
-    </Row>
+    <Header />
   );
 };
