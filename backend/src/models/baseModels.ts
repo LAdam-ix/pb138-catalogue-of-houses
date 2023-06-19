749e1af64c77528b47c4a64a4265d6ef596e277c
Adam <xlopatka@fi.muni.cz>
2023-06-20 01:30:47 +0200

    feat: add validation error messages

--- backend/src/models/baseModels.ts
+++ backend/src/models/baseModels.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 
 export const ParamsWithIdSchema = z.object({
-  id: z.string({ required_error: 'Missing `id` in url parameters' }).nonempty(),
+  id: z.string().uuid('Invalid UUID format').nonempty('ID is required'),
 }).strict();
 
 export const base64ImageSchema = z.string().refine((value) => {
@@ -10,4 +10,8 @@ export const base64ImageSchema = z.string().refine((value) => {
   return base64Regex.test(value);
 }, {
   message: 'Invalid base64 image',
+});
+
+export const requiredErr = (fieldName: string) => ({
+  required_error: `Property \`${fieldName}\` is required`,
 });
--- backend/src/models/orderModels.ts
+++ backend/src/models/orderModels.ts
@@ -1,14 +1,15 @@
 import { z } from 'zod';
+import { requiredErr } from './baseModels';
 
 const OrderPostSchema = z.object({
-  price: z.number(),
-  houseId: z.string(),
-  designerId: z.string(),
-  location: z.string(),
+  price: z.number(requiredErr('price')).min(0, 'Price must be a non-negative number'),
+  houseId: z.string(requiredErr('houseId')).uuid('Invalid UUID format').nonempty('House ID is required'),
+  designerId: z.string(requiredErr('designerId')).uuid('Invalid UUID format').nonempty('Designer ID is required'),
+  location: z.string(requiredErr('location')).nonempty('Location is required'),
 }).strict();
 
 const OrderPatchSchema = z.object({
-  location: z.string(),
+  location: z.string(requiredErr('location')).nonempty('Location is required'),
 }).strict();
 
 export { OrderPostSchema, OrderPatchSchema };
