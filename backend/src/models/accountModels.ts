8113afd339a70aa0460024c1a21375af22818eda
Adam <xlopatka@fi.muni.cz>
2023-06-20 14:36:43 +0200

    feat: improve validation and error messages

--- backend/src/models/accountModels.ts
+++ backend/src/models/accountModels.ts
@@ -3,22 +3,22 @@ import { AccountTypeEnumeration } from '../enumTypes';
 import { base64ImageSchema, requiredErr } from './baseModels';
 
 const AccountRegistationScheme = z.object({
-  email: z.string(requiredErr('email')).email('Invalid email format').nonempty('Email is required'),
-  name: z.string(requiredErr('name')).nonempty('Name is required'),
-  surname: z.string(requiredErr('surname')).nonempty('surname is required'),
-  password: z.string(requiredErr('password')).min(8, 'Password must be at least 8 characters').nonempty('Password is required'),
+  email: z.string(requiredErr('email')).email('Invalid email format').nonempty('Email must not be empty'),
+  name: z.string(requiredErr('name')).nonempty('Name must not be empty'),
+  surname: z.string(requiredErr('surname')).nonempty('Surname must not be empty'),
+  password: z.string(requiredErr('password')).min(8, 'Password must be at least 8 characters').nonempty('Password must not be empty'),
   type: z.enum(AccountTypeEnumeration,requiredErr('type')),
 }).strict();
 
 const AccountLoginScheme = z.object({
-  email: z.string(requiredErr('email')).email('Invalid email format').nonempty('Email is required'),
-  password: z.string(requiredErr('password')).nonempty('Password is required'),
+  email: z.string(requiredErr('email')).email('Invalid email format').nonempty('Email must not be empty'),
+  password: z.string(requiredErr('password')).nonempty('Password must not be empty'),
 }).strict();
 
 const AccountPatchSchema = z.object({
   email: z.string().email('Invalid email format').optional(),
-  name: z.string().optional(),
-  surname: z.string().optional(),
+  name: z.string().nonempty('Name must not be empty').optional(),
+  surname: z.string().nonempty('Surname must not be empty').optional(),
   avatar: base64ImageSchema.optional(),
   password: z.string().min(8, 'Password must be at least 8 characters').optional(),
   type: z.enum([
--- backend/src/models/houseModels.ts
+++ backend/src/models/houseModels.ts
@@ -3,18 +3,24 @@ import { HouseOrdering, HouseTypeEnumeration } from '../enumTypes';
 import { base64ImageSchema, requiredErr } from './baseModels';
 
 const HousePostSchema = z.object({
-  name: z.string(requiredErr('name')).nonempty('Name is required'),
+  name: z.string(requiredErr('name')).nonempty('Name must not be empty'),
   description: z.string(requiredErr('description')), // can be empty string
   images: z.array(base64ImageSchema).nonempty('At least one image is required'),
   price: z.number(requiredErr('price')).min(0, 'price must be a non-negative number'),
-  type: z.enum(HouseTypeEnumeration,requiredErr('type')),
+  type: z.enum(HouseTypeEnumeration, requiredErr('type')),
 }).strict();
 
 const HousePatchSchema = z.object({
-  name: z.string().nonempty('Name is required').optional(),
+  name: z.string().nonempty('Name must not be empty').optional(),
   description: z.string().optional(),
-  customerId: z.string().uuid().optional(),
-  designerId: z.string().optional(),
+  customerId: z.string()
+    .uuid('Invalid UUID format')
+    .nonempty('Customer ID must not be empty')
+    .optional(),
+  designerId: z.string()
+    .uuid('Invalid UUID format')
+    .nonempty('Designer ID must not be empty')
+    .optional(),
   images: z.array(base64ImageSchema),
   price: z.number().min(0, 'price must be a non-negative number').optional(),
   type: z.enum(HouseTypeEnumeration).optional(),
@@ -30,7 +36,7 @@ const HouseGetMultiSchema = z.object({
   maxPrice: z.string().refine((val) => parseFloat(val) > 0, {
     message: 'MaxPrice must be a positive number',
   }).optional(),
-  searchName: z.string().nonempty('At least one image is required').optional(),
+  searchName: z.string().nonempty('At least one image must not be empty').optional(),
 }).strict() || undefined;
 
 export { HousePostSchema, HousePatchSchema, HouseGetMultiSchema };
