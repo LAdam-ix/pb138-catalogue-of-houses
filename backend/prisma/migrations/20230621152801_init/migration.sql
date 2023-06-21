cb35baf4f029f740f8aadf263ddfe59b3f3f23e4
Adam <xlopatka@fi.muni.cz>
2023-06-21 17:36:12 +0200

    refactor: clean dev prisma migration

--- backend/prisma/migrations/20230621152801_init/migration.sql
+++ backend/prisma/migrations/20230621152801_init/migration.sql
@@ -1,15 +1,24 @@
 -- CreateTable
+CREATE TABLE "ImageLink" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "path" TEXT NOT NULL,
+    "houseId" TEXT,
+    CONSTRAINT "ImageLink_houseId_fkey" FOREIGN KEY ("houseId") REFERENCES "House" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "surename" TEXT NOT NULL,
-    "avatar" TEXT,
+    "surname" TEXT NOT NULL,
+    "avatarId" TEXT,
     "hashedPassword" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "deletedAt" DATETIME,
     "type" TEXT NOT NULL,
+    CONSTRAINT "Account_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "ImageLink" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT "Account_type_fkey" FOREIGN KEY ("type") REFERENCES "AccountType" ("type") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -22,10 +31,8 @@ CREATE TABLE "AccountType" (
 CREATE TABLE "House" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "type" TEXT NOT NULL,
-    "pictureLink" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "modelLink" TEXT NOT NULL,
-    "cost" REAL NOT NULL,
+    "price" REAL NOT NULL,
     "description" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -43,15 +50,15 @@ CREATE TABLE "HouseType" (
 -- CreateTable
 CREATE TABLE "Order" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT,
+    "price" REAL NOT NULL,
     "location" TEXT NOT NULL,
-    "budget" REAL NOT NULL,
-    "description" TEXT NOT NULL,
+    "houseId" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "deletedAt" DATETIME,
     "customerId" TEXT NOT NULL,
     "designerId" TEXT NOT NULL,
+    CONSTRAINT "Order_houseId_fkey" FOREIGN KEY ("houseId") REFERENCES "House" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Order_designerId_fkey" FOREIGN KEY ("designerId") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -73,7 +80,7 @@ CREATE TABLE "Rating" (
 -- CreateIndex
 CREATE UNIQUE INDEX "Account_email_key" ON "Account"("email");
 
-
+-- AddEnums
 BEGIN TRANSACTION;
 INSERT INTO "HouseType" ("type") VALUES ("FAMILY_HOUSE");
 INSERT INTO "HouseType" ("type") VALUES ("BUNGALOW");
