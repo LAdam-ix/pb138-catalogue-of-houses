8468a7f270346745cb7b06141b037c81a55cdbe7
Michal Duda <xduda@fi.muni.cz>
2023-06-21 09:37:15 +0200

    fix: move AuthAPI properly

--- frontend/src/services/AuthAPI.ts
+++ frontend/src/services/AuthAPI.ts
@@ -2,9 +2,13 @@ import { FieldValues } from "react-hook-form";
 import axiosInstance from "./base";
 
 export const registerAccount = async (data: FieldValues) => {
-    return await axiosInstance.post('/auth/registration', data);
+    return (await axiosInstance.post('/auth/registration', data)).data;
 }
 
 export const loginAccount = async (data: FieldValues) => {
-    return await axiosInstance.post("/auth/login", data);
+    return (await axiosInstance.post("/auth/login", data)).data;
+}
+
+export const authAccount = async () => {
+    return (await axiosInstance.get("/auth", {})).data;
 }
