c2b9052df9848d007a43d8ffaa78cd4e3b97dc3b
Adam <xlopatka@fi.muni.cz>
2023-06-21 17:26:33 +0200

    feat: add data-seeding (WIP images)

--- backend/src/seed/seed.ts
+++ backend/src/seed/seed.ts
@@ -1,16 +1,56 @@
 import prisma from '../client';
-// import data from './data';
+import { accounts, houses, imageLinksAccount, imageLinksHouses, ratings, orders } from './data';
 
 const seed = async () => {
   console.log(`[${new Date().toISOString()}] Seed started`);
-  // seeding is here
+
+  // Create ImageLinks
+  for (const imageLink of imageLinksAccount) {
+    await prisma.imageLink.create({
+      data: imageLink,
+    });
+  }
+
+  // Create Accounts
+  for (const account of accounts) {
+    await prisma.account.create({
+      data: account,
+    });
+  }
+
+  // Create Houses
+  for (const house of houses) {
+    await prisma.house.create({
+      data: house,
+    });
+  }
+  for (const imageLink of imageLinksHouses) {
+    await prisma.imageLink.create({
+      data: imageLink,
+    });
+  }
+  // Create Ratings
+  for (const rating of ratings) {
+    await prisma.rating.create({
+      data: rating,
+    });
+  }
+
+  // Create Orders
+  for (const order of orders) {
+    await prisma.order.create({
+      data: order,
+    });
+  }
+
+  console.log(`[${new Date().toISOString()}] Seed succeeded`);
 };
 
-seed().then(() => {
-  console.log(`[${new Date().toISOString()}] Seed succeeded`);
-}).catch((e) => {
-  console.log(`[${new Date().toISOString()}] Seed failed`);
-  console.log(e);
-}).finally(async () => {
-  await prisma.$disconnect();
-});
+seed()
+  .catch((e) => {
+    console.log(`[${new Date().toISOString()}] Seed failed`);
+    console.log(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
