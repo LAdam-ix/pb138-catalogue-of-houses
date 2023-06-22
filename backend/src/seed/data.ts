057cefc8838d39d91373804ccaa8c77e22dfa614
Adam <xlopatka@fi.muni.cz>
2023-06-22 02:30:27 +0200

    fix: hashed passwords

--- backend/src/seed/data.ts
+++ backend/src/seed/data.ts
@@ -1,8 +1,12 @@
+const password1 = '$argon2id$v=19$m=65536,t=3,p=4$6Qq/pFkY2H1DmUW8lsY2TA$U1i0yLPHZRtmXXQuGZbsbG9shDrIBrUJbaG+uKa6Xrw';
+const password2 = '$argon2id$v=19$m=65536,t=3,p=4$ruZBK1xDe88hHOhvsjO4Yw$Al+yjmmREcvBZfytLr6S5oES80rex7oD3bqr8rtdu+E';
+const password3 = '$argon2id$v=19$m=65536,t=3,p=4$9JorQa1FsTZ2EIKh0++icg$Z2oeKBHJ0R/faajF79wEoYGwd3wUpaYzUVc7X80OlXY';
 
 export const imageLinksAccount = [
   { id: 'd1ce26af-1c65-1e5a-86a0-ec5a797d3d0f', path: 'path1', },
   { id: 'd5ae26af-86a0-4e5a-1c65-cc2a1f94a1a7', path: 'path1', },
 ];
+
 export const accounts = [
   {
     id: 'f1fc5ae4-1c65-4e5a-86a0-ec5a797d3d0f',
@@ -10,7 +14,7 @@ export const accounts = [
     name: 'John',
     surname: 'Doe',
     avatarId: imageLinksAccount[0].id,
-    hashedPassword: 'password1',
+    hashedPassword: password1,
     type: 'USER',
     createdAt: new Date(2023, 6, 20),
   },
@@ -19,7 +23,7 @@ export const accounts = [
     email: 'user2@example.com',
     name: 'Jane',
     surname: 'Smith',
-    hashedPassword: 'password2',
+    hashedPassword: password2,
     type: 'USER',
     createdAt: new Date(2023, 6, 19),
   },
@@ -29,7 +33,7 @@ export const accounts = [
     name: 'Emily',
     surname: 'Brown',
     avatarId: imageLinksAccount[1].id,
-    hashedPassword: 'password3',
+    hashedPassword: password3,
     type: 'DESIGNER',
     createdAt: new Date(2023, 6, 18),
   },
@@ -44,7 +48,7 @@ export const houses = [
     price: 300000,
     description: 'Spacious family house',
     createdAt: new Date(2023, 6, 17), // Four days ago
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    designerId: accounts[2].id,
   },
   {
     id: '46fb3669-b6de-4b64-8949-d2bb3a35fc71',
@@ -54,7 +58,7 @@ export const houses = [
     price: 200000,
     description: 'Cozy bungalow',
     createdAt: new Date(2023, 6, 16),
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    designerId: accounts[2].id,
   },
 ];
 
@@ -72,32 +76,32 @@ export const ratings = [
     score: 8,
     comment: 'Great designer!',
     createdAt: new Date(2023, 6, 20),
-    customerId: 'f1fc5ae4-1c65-4e5a-86a0-ec5a797d3d0f',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[0].id,
+    designerId: accounts[2].id,
   },
   {
     id: '79e3f3ef-0642-40c6-a671-1a5a2223c45c',
     score: 9,
     comment: 'Excellent work!',
     createdAt: new Date(2023, 6, 19),
-    customerId: 'f1fc5ae4-1c65-4e5a-86a0-ec5a797d3d0f',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[0].id,
+    designerId: accounts[2].id,
   },
   {
     id: '7e59a690-96c2-48e0-9d18-ee74507a9e3d',
     score: 7,
     comment: 'Good job!',
     createdAt: new Date(2023, 6, 18),
-    customerId: 'a32a8ea3-283b-4ad9-9a3f-cc2a1f94a1a7',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[1].id,
+    designerId: accounts[2].id,
   },
   {
     id: '37feac4c-6225-4014-91de-0497b10b7f06',
     score: 6,
     comment: 'Could be better.',
     createdAt: new Date(2023, 6, 17),
-    customerId: 'a32a8ea3-283b-4ad9-9a3f-cc2a1f94a1a7',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[1].id,
+    designerId: accounts[2].id,
   },
 ];
 
@@ -106,18 +110,18 @@ export const orders = [
     id: '2b619a12-7727-4f0f-90ef-64a8c1d7a81e',
     price: 150000,
     location: 'New York',
-    houseId: 'd1ce26af-ef39-4a12-b434-849be3589fb2',
+    houseId: houses[0].id,
     createdAt: new Date(2023, 6, 21), // One day ago
-    customerId: 'f1fc5ae4-1c65-4e5a-86a0-ec5a797d3d0f',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[0].id,
+    designerId: accounts[2].id,
   },
   {
     id: 'd41c2b6e-22a7-4fb4-941d-88d019f10213',
     price: 250000,
     location: 'Los Angeles',
-    houseId: '46fb3669-b6de-4b64-8949-d2bb3a35fc71',
+    houseId: houses[1].id,
     createdAt: new Date(2023, 6, 21), // Two days ago
-    customerId: 'a32a8ea3-283b-4ad9-9a3f-cc2a1f94a1a7',
-    designerId: '9a84618f-84ef-45b9-9eae-4a256118e1ce',
+    customerId: accounts[1].id,
+    designerId: accounts[2].id,
   },
 ];
