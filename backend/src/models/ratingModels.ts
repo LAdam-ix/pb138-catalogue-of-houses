2d274bbcae6976978d20fdd1577d8706e9833d06
Adam <xlopatka@fi.muni.cz>
2023-06-22 09:17:58 +0200

    fix: max 5 score

--- backend/src/models/ratingModels.ts
+++ backend/src/models/ratingModels.ts
@@ -3,7 +3,7 @@ import { requiredErr } from './baseModels';
 
 const score = z.number(requiredErr('score'))
   .min(0, 'Score must be a non-negative number')
-  .max(10, 'Score cannot exceed 10');
+  .max(5, 'Score cannot exceed 5');
 
 const designerId = z.string(requiredErr('designerId'))
   .uuid('Invalid UUID format')
